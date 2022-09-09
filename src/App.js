c56d42f430080ab44869941823839fe06a4fd51a
ilgyha228 <ilgyha228@gmail.com>
2022-09-09 22:12:58 +0300

    functionality(sort)

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@
 import PostForm from "./components/PostForm";
  
 import PostList from "./components/PostList";
+import MySelect from "./components/UI/select/MySelect";
  
  import  './css styles/App.css';
 function App() {
     const[post,setPost] = useState([
-      {id:1, title: 'first post', body:'description'},
-      {id:2, title: 'second post', body:'description'},
-      {id:3, title: 'third post', body:'description'},
+      {id:1, title: 'aa', body:'bb'},
+      {id:2, title: 'gg', body:'aa'},
+      {id:3, title: 'bb', body:'ww'},
     ])
       
+    const [selectedSort,setSelectedSort] = useState('')
+
     const createPost = (newPost) =>{
         setPost([...post,newPost])
     }
@@ -19,9 +22,26 @@ function App() {
     setPost(post.filter(p => p.id !== posts.id))
    }
 
+  const sortPost = (sort) => {
+    setSelectedSort(sort)
+    setPost([...post].sort((a , b) => a[sort].localeCompare(b[sort])))
+  }
+
   return (
     <div className="App">
       <PostForm create={createPost}/>
+      <hr style={{margin:'15px 0'}}/>
+      <div>
+        <MySelect
+        value={selectedSort}
+        onChange={sortPost}
+        defaultValue="Sort by"
+        options={[
+          {value:'title',name:'Sort by name'},
+          {value:'body',name:'Sort by description'},
+        ]}
+        />
+      </div>
       {post.length !==0
       ? 
       <PostList remove={removePost} post={post} title="List of Posts"/>
